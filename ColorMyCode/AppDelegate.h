f0d38e1d8e32c74fee1adc44fd4701db7744177a
hzyuxiaohua <hzyuxiaohua@corp.netease.com>
2018-01-21 17:29:49 +0800

    Initial Commit

---
+++ ColorMyCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorMyCode
+//
+//  Created by hzyuxiaohua on 21/01/2018.
+//  Copyright © 2018 hzyuxiaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorMyCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorMyCode
+//
+//  Created by hzyuxiaohua on 21/01/2018.
+//  Copyright © 2018 hzyuxiaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
